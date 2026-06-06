d48eda42a8b78b7c355e1e496bae69cb5dd81419
Paulo Emanuel Duarte de Freitas <pauloe.duarte1@gmail.com>
2026-06-06 19:33:32 -0300

    atualização do model e do do view

--- Sistema-gerenciamento-pedidos/model/Produto.java
+++ Sistema-gerenciamento-pedidos/model/Produto.java
@@ -5,54 +5,47 @@ package model;
  * @author vitor
  */
 
+package model;
+
 public class Produto {
-    private int cod_Produto;
+
+    private int codProduto;
     private String nome;
     private float preco;
-    private int Qtdeproduto;
+    private int qtdeProduto;
+
+    public Produto() {}
+
+    public Produto(int codProduto, String nome, float preco, int qtdeProduto) {
+        this.codProduto   = codProduto;
+        this.nome         = nome;
+        this.preco        = preco;
+        this.qtdeProduto  = qtdeProduto;
+    }
+
+    // Getters
+    public int getCodProduto()    { return codProduto; }
+    public String getNome()       { return nome; }
+    public float getPreco()      
+    { return preco; }
+    public int getQtdeProduto()   { return qtdeProduto; }
+
+    // Setters 
+    public void setNome(String nome)            
+    { this.nome = nome; }
     
-    public Produto(){}
+    public void setPreco(float preco)          
+    { this.preco = preco; }
     
-    public Produto(int cod_Porduo, String nome, float preco, int QtdeProdutow ){
-        this.cod_Produto = cod_Produto;
-        this.nome = nome;
-        this.preco = preco;
-        this.Qtdeproduto = Qtdeproduto;
-    }
-    
-    //getters
-    public int Getcod_Produto(){
-        return cod_Produto;
-    }
-    public String Getnome(){
-        return nome;
-    }
-    public float Getpreco(){
-        return preco;
-    }
-    public int GetQtdeproduto(){
-        return Qtdeproduto;
-    }
-    
-    //setters
-    public void Setcod_Produto( int cod_Produto){
-            this.cod_Produto = cod_Produto;
-    }
-    
-    public void Setnome(String nome){
-        this.nome = nome;
-    }
-    
-    public void Setpreco( float preco){
-        this.preco = preco;
-    }
-    
-    public void SetQtdeproduto( int Qtdeproduto){
-        this.Qtdeproduto = Qtdeproduto;
-    }
-    
+    public void setQtdeProduto(int qtdeProduto) 
+    { this.qtdeProduto = qtdeProduto; }
+
     @Override
     public String toString() {
-    return cod_Produto + ";" + nome + ";" + preco + ";" + Qtdeproduto;
+        return codProduto + ";" + nome + ";" + preco + ";" + qtdeProduto;
+    }
+
+    public String toCSV() {
+        return toString();
+    }
 }
- }
